95d23d0510944e5b06224b225b0df199b0781055
JonathanKosasih18 <jonathankosasih@gmail.com>
2025-05-04 00:40:30 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'jk-white': '#F7F7F7',
+        'jk-black': '#1F1F1F',
+        'jk-gray': '#929AAB',
+        'jk-text-white': '#EEEEEE',
+        'jk-text-black': '#1F1F1F',
+        'jk-lighter-black': '#2A2A2A',
+        'jk-sky': '#00A3E0',
+        'jk-dark-gray': '#1A1A1A',
+        'jk-light-gray': '#98AFC7',
+      },
+    },
+  },
+  plugins: [],
+}
